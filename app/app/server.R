216f4f32e2498ea06b6452f05a04dd08546517dc
liiskolb <liiskolb@ut.ee>
2017-04-26 12:42:27 +0300

    Infoleht

--- app/app/server.R
+++ app/app/server.R
@@ -2,8 +2,12 @@ library(dplyr)
 library(ggplot2)
 library(wordcloud)
 library(scales)
+library(reshape2)
 
 cleandata <- read.csv("~/Desktop/DOK/K_2017/Andmete_visualiseerimine/Projekt/sajv_projekt/data/meediadata_cleaned1.csv")
+cleandata$postag_descriptions <-factor(cleandata$postag_descriptions, levels=c("nimisõna", "tegusõna", "pärisnimi", "lausemärk", "määrsõna", "omadussõna algvõrre", "asesõna", "sidesõna", "kaassõna", "põhiarvsõna", "lühend", "omadussõna keskvõrre", "järgarvsõna", "omadussõna ülivõrre", "käändumatu omadussõna", "verbi juurde kuuluv sõna", "hüüdsõna", ""))
+# eemalda tühjad postagid
+cleandata <- cleandata %>% subset(postag_descriptions!="")
 
 colors <- c("#ffc820", "#0054a6", "#cf0007", "#00adee") # delfi, err, ohtuleht, postimees
 allikad <- c("delfi", "err", "ohtuleht", "postimees")
@@ -12,33 +16,63 @@ ilus_allikad <- c("Delfi", "ERR", "Õhtuleht", "Postimees")
 
 
 sonade_jaotus = function(cleandata, sonaliik, colors, allikad, ilus_allikad){
-  subdata <- cleandata %>% select(postags, allikas) %>%
-    group_by(allikas, postags) %>%
-    summarise (n = n()) %>%
-    mutate(freq = n / sum(n)) %>% subset(postags==sonaliik) 
-  p <- ggplot(subdata, aes(x=allikas, y=freq, fill=allikas)) + geom_bar(stat="identity") + scale_y_continuous(labels=percent) +
-    theme_bw() + scale_fill_manual(breaks = allikad, values=colors, labels=ilus_allikad) + labs(y="Osakaal") + theme(axis.title.x=element_blank(), axis.text.x=element_blank(),
-                                                                                                                               axis.ticks.x=element_blank(), legend.title=element_blank(), legend.position="bottom")
+  if (sonaliik=="koik"){
+    subdata <- cleandata %>% select(postag_descriptions, allikas) %>%
+      group_by(allikas, postag_descriptions) %>%
+      summarise (n = n()) %>%
+      mutate(freq = n / sum(n))
+    data.m <- melt(subdata, id.vars=c('postag_descriptions', 'allikas'), measure.vars='freq')
+    p <- ggplot(data.m, aes(x=postag_descriptions, y=value, fill=allikas)) + geom_bar(stat="identity", position = "dodge") + scale_y_continuous(labels=percent) +
+      theme_bw() + scale_fill_manual(breaks = allikad, values=colors, labels=ilus_allikad) + labs(y="Osakaal") + theme(axis.title.x=element_blank(), axis.text.x = element_text(angle = 90, hjust = 1),                                                                                                                       axis.ticks.x=element_blank(), legend.title=element_blank(), legend.position="bottom")
+  }
+  else{
+    subdata <- cleandata %>% select(postags, allikas) %>%
+      group_by(allikas, postags) %>%
+      summarise (n = n()) %>%
+      mutate(freq = n / sum(n)) %>% subset(postags==sonaliik) 
+    p <- ggplot(subdata, aes(x=allikas, y=freq, fill=allikas)) + geom_bar(stat="identity") + scale_y_continuous(labels=percent) +
+      theme_bw() + scale_fill_manual(breaks = allikad, values=colors, labels=ilus_allikad) + labs(y="Osakaal") + theme(axis.title.x=element_blank(), axis.text.x=element_blank(),
+                                                                                                                       axis.ticks.x=element_blank(), legend.title=element_blank(), legend.position="bottom")
+    
+  }
+  
   return(p)                                                                                                               
 }
 
 plot_wordcloud <- function(cleandata, sonaliik){
-  subdata <- cleandata %>% select(postags, allikas, lemmas) %>% group_by(lemmas) %>% mutate(count = n()) %>% subset(postags==sonaliik) %>% distinct(lemmas, .keep_all = TRUE)
-  wc <- wordcloud(subdata$lemmas, subdata$count, max.words = 100, min.freq = 1, random.order=TRUE, colors=brewer.pal(8,"Dark2"), random.color=TRUE)
+  if (sonaliik=="koik"){
+    subdata <- cleandata %>% select(postags, allikas, lemmas) %>% group_by(lemmas) %>% mutate(count = n()) %>% distinct(lemmas, .keep_all = TRUE)
+  }
+  else{
+    subdata <- cleandata %>% select(postags, allikas, lemmas) %>% group_by(lemmas) %>% mutate(count = n()) %>% subset(postags==sonaliik) %>% distinct(lemmas, .keep_all = TRUE)
+  }
+  wc <- wordcloud(subdata$lemmas, subdata$count, max.words = 200, min.freq = 1, random.order=TRUE, colors=brewer.pal(8,"Dark2"), random.color=FALSE)
   return(wc)
 }
 
 top_sonad = function(cleandata, sonaliik, source, colors){
-  subdata <- cleandata %>% select(postags, allikas, lemmas) %>% subset(allikas==source & postags==sonaliik) %>%
-    group_by(lemmas) %>% mutate(count = n()) %>% filter(row_number(lemmas) == 1) %>% ungroup() %>% mutate(lemmas=as.character(lemmas))
+  if (sonaliik=="koik"){
+    subdata <- cleandata %>% select(postags, allikas, lemmas) %>% subset(allikas==source) %>%
+      group_by(lemmas) %>% mutate(count = n()) %>% filter(row_number(lemmas) == 1) %>% ungroup() %>% mutate(lemmas=as.character(lemmas))
+  }
+  else{
+    subdata <- cleandata %>% select(postags, allikas, lemmas) %>% subset(allikas==source & postags==sonaliik) %>%
+      group_by(lemmas) %>% mutate(count = n()) %>% filter(row_number(lemmas) == 1) %>% ungroup() %>% mutate(lemmas=as.character(lemmas))
+  }
   top10 <- top_n(arrange(subdata, desc(count)), 10, count)
   p <- ggplot(top10, aes(x=reorder(as.factor(lemmas), count), y=count)) + geom_bar(stat="identity", fill=colors[[source]]) + theme_bw() + coord_flip() + xlab("Sõna") + ylab("Sagedus")
   return(p)
 }
 
 unikaalsed_sonad = function(cleandata, sonaliik, colors, source){
+  if(sonaliik=="koik"){
+    subdata <- cleandata %>% select(postags, allikas, lemmas, word_texts) %>% group_by(allikas, lemmas) %>%
+      distinct(allikas, lemmas, .keep_all = TRUE) %>%  group_by(lemmas) %>% mutate(count = n()) %>% filter(count==1)
+  }
+  else{
   subdata <- cleandata %>% select(postags, allikas, lemmas, word_texts) %>% subset(postags==sonaliik) %>% group_by(allikas, lemmas) %>%
     distinct(allikas, lemmas, .keep_all = TRUE) %>%  group_by(lemmas) %>% mutate(count = n()) %>% filter(count==1) 
+  }
   wc <- wordcloud(subdata[subdata$allikas==source, ]$lemmas, subdata[subdata$allikas==source,]$count, scale=c(1,.2), random.order=TRUE, rot.per=0.1, colors=colors[[source]],random.color=FALSE, max.words=60)
   return(wc)
 }
--- app/app/ui.R
+++ app/app/ui.R
@@ -7,31 +7,12 @@ header <- dashboardHeader(title = "Eesti Meedia", disable = FALSE)
 
 # Sidebar elements for the search visualizations
 sidebar <- dashboardSidebar(
-   
   sidebarMenu(
-    selectInput("sonaliik", "Vali sõnaliik:",
-                c("Nimisõna" = "S",
-                  "Tegusõna" = "V", 
-                  "Asesõna" = "P",
-                  "Hüüdsõna" = "I",
-                  "Järgarvsõna" = "O",
-                  "Käändamatu omadussõna" = "G",
-                  "Kaassõna" = "K",
-                  "Lausemärk" = "Z",
-                  "Lühend" = "Y",
-                  "Määrsõna" = "D",
-                  "Omadussõna algvõrre" = "A",
-                  "Omadussõna keskvõrre" = "C",
-                  "Omadussõna ülivõrre" = "U",
-                  "Pärisnimi" = "H", 
-                  "Põhiarvsõna" = "N",
-                  "Sidesõna" = "J",
-                  "Verbi juurde kuuluv sõna" = "X"
-                  ))
-
-    # this is where other menuItems & menuSubItems would go
-  ) # /sidebarMenu
-) # /dashboardSidebar
+    menuItem("Sõnade ülevaade", tabName = "dashboard", icon = icon("bar-chart")),
+    menuItem("Lause analüüs", icon = icon("area-chart"), tabName = "widgets"),
+    menuItem("Meist", tabName = "info", icon = icon("info"))
+  )
+)
 
 #Body elements for the search visualizations.
 body <- dashboardBody(
@@ -44,30 +25,77 @@ body <- dashboardBody(
                     }
                     '))
   ),
-  fluidRow(
-    box(title="Sõnaliigi jaotus", plotOutput("jaotus"), background = "purple"),
-    box(title="Sõnapilv", plotOutput("sonapilv"), background = "purple")
-  ),
-  fluidRow(
-    tabBox(
-      title = "10 populaarseimat sõna",
-      # The id lets us use input$tabset1 on the server to find the current tab
-      id = "tabset1",
-      tabPanel("Delfi", plotOutput("top_delfi")),
-      tabPanel("ERR", plotOutput("top_err")),
-      tabPanel("Õhtuleht", plotOutput("top_ohtuleht")),
-      tabPanel("Postimees", plotOutput("top_postimees"))
+  tabItems(
+    tabItem(tabName = "info",
+            h3("Tähelepanu!"),
+            br(),
+            div(p("Tegemist on Tartu Ülikooli aine \"Statistiline andmeteadus ja visualiseerimine\" raames loodud rakendusega.")),
+            br(),
+            p("Alates 12.aprillist 2017 on kord päevas kogutud artiklite pealkirju neljast Eesti uudiseportaalist:"),
+            HTML("<ul><li>Delfi</li><li>ERR</li><li>Õhtuleht</li><li>Postimees</li></ul>"),
+            p("Kogutud lausetest on esitatud mitmesugused ülevaatlikud visualisatsioonid."),
+            br(),br(),
+            p(HTML("<b>Kaebuste tekkimise korral pidage nõu Liis Kolbergi või Mari-Liis Allikiviga.</b>"))
     ),
-    tabBox(
-      title = "Unikaalsed sõnad",
-      # The id lets us use input$tabset1 on the server to find the current tab
-      id = "tabset2",
-      tabPanel("Delfi", plotOutput("uni_delfi")),
-      tabPanel("ERR", plotOutput("uni_err")),
-      tabPanel("Õhtuleht", plotOutput("uni_ohtuleht")),
-      tabPanel("Postimees", plotOutput("uni_postimees"))
+    tabItem(tabName = "dashboard",
+            h2("Pealkirjades esinevate sõnade ülevaade"),
+            fluidRow(
+              box(
+                selectInput("sonaliik", "Vali sõnaliik mida kuvada:",
+                            c("Kõik sõnad" = "koik",
+                              "Nimisõna" = "S",
+                              "Tegusõna" = "V",
+                              "Asesõna" = "P",
+                              "Hüüdsõna" = "I",
+                              "Järgarvsõna" = "O",
+                              "Käändamatu omadussõna" = "G",
+                              "Kaassõna" = "K",
+                              "Lausemärk" = "Z",
+                              "Lühend" = "Y",
+                              "Määrsõna" = "D",
+                              "Omadussõna algvõrre" = "A",
+                              "Omadussõna keskvõrre" = "C",
+                              "Omadussõna ülivõrre" = "U",
+                              "Pärisnimi" = "H",
+                              "Põhiarvsõna" = "N",
+                              "Sidesõna" = "J",
+                              "Verbi juurde kuuluv sõna" = "X"
+                            )
+                )
+              )
+            ),
+            fluidRow(
+              box(title="Sõnaliigi jaotus", plotOutput("jaotus"), background = "purple"),
+              box(title="Sõnapilv", plotOutput("sonapilv"), background = "purple")
+            ),
+            fluidRow(
+              tabBox(
+                title = "10 populaarseimat sõna",
+                # The id lets us use input$tabset1 on the server to find the current tab
+                id = "tabset1",
+                tabPanel("Delfi", plotOutput("top_delfi")),
+                tabPanel("ERR", plotOutput("top_err")),
+                tabPanel("Õhtuleht", plotOutput("top_ohtuleht")),
+                tabPanel("Postimees", plotOutput("top_postimees"))
+              ),
+              tabBox(
+                title = "Unikaalsed sõnad",
+                # The id lets us use input$tabset1 on the server to find the current tab
+                id = "tabset2",
+                tabPanel("Delfi", plotOutput("uni_delfi")),
+                tabPanel("ERR", plotOutput("uni_err")),
+                tabPanel("Õhtuleht", plotOutput("uni_ohtuleht")),
+                tabPanel("Postimees", plotOutput("uni_postimees"))
+              )
+            )
+            
+    ),
+    
+    tabItem(tabName = "widgets",
+            h2("Pealkirjade analüüs")
     )
   )
+  
 ) # /dashboardBody
 
 
